b8a6be9f4595cfca9730b0f982971e4802261f4a
Taras Tiurin <taras.tiurin@eleks.com>
2022-12-07 23:21:23 +0200

    Replace ezyang/htmlpurifier (LGPL2.1) with symfony/html-sanitizer

--- src/PhpSpreadsheet/Writer/Html.php
+++ src/PhpSpreadsheet/Writer/Html.php
@@ -2,7 +2,8 @@
 
 namespace PhpOffice\PhpSpreadsheet\Writer;
 
-use HTMLPurifier;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
 use PhpOffice\PhpSpreadsheet\Calculation\Calculation;
 use PhpOffice\PhpSpreadsheet\Cell\Cell;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
@@ -1770,12 +1771,10 @@ class Html extends BaseWriter
     {
         $result = '';
         if (!$this->isPdf && isset($worksheet->getComments()[$coordinate])) {
-            $sanitizer = new HTMLPurifier();
-            $cachePath = File::sysGetTempDir() . '/phpsppur';
-            if (is_dir($cachePath) || mkdir($cachePath)) {
-                $sanitizer->config->set('Cache.SerializerPath', $cachePath);
-            }
-            $sanitizedString = $sanitizer->purify($worksheet->getComment($coordinate)->getText()->getPlainText());
+            $config = (new HtmlSanitizerConfig())->allowSafeElements();
+            $sanitizer = new HtmlSanitizer($config);
+
+            $sanitizedString = $sanitizer->sanitize($worksheet->getComment($coordinate)->getText()->getPlainText());
             if ($sanitizedString !== '') {
                 $result .= '<a class="comment-indicator"></a>';
                 $result .= '<div class="comment">' . nl2br($sanitizedString) . '</div>';
